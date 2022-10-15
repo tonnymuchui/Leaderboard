fadad019d8ae9560770417ea4cd587b8bbee8939
tonnymuchui <tonykanyingah@gmail.com>
2022-10-15 18:23:33 +0300

    Add javascript file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 // css files here
 import './style.css';
+import Score from './modules/Api.js';
+
+const newScore = new Score();
+const addNewScore = document.querySelector('.add-new-score');
+addNewScore.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const user = addNewScore.name.value;
+  const scoreNum = addNewScore.score.value;
+  newScore.addScore({ user, scoreNum });
+  addNewScore.name.value = '';
+  addNewScore.score.value = '';
+});
+
+const refreshButton = document.querySelector('.refresh-button');
+refreshButton.addEventListener('click', newScore.fetchScore);
